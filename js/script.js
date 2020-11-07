c8b8c34fc2aa8b0d2c2518d14baaad65968e3668
RitaSilva28-github <63984197+RitaSilva28@users.noreply.github.com>
2020-11-07 04:54:24 +0000

    added files

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var about = document.getElementById("about-button");
+var buttonHome = document.getElementById("button-home");
+
+about.addEventListener("click", function () {
+  removeClassActive();
+});
+
+function removeClassActive() {
+
+  buttonHome.classList.remove("active");
+  about.classList.add("active");
+
+
+};
